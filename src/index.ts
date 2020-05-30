ee07b2b5202ac2a37ac82010377cd2b5ba098fcd
zcong1993 <zhangcong1992@gmail.com>
2020-05-31 00:14:35 +0800

    perf: improve fallback performance

--- src/index.ts
+++ src/index.ts
@@ -22,26 +22,25 @@ export function withDefaultValue<T>(value: T) {
 export async function aggregator<T extends [unknown, ...unknown[]]>(
   iterable: HandlerTuple<T>
 ): Promise<ResultTuple<T>> {
-  const resArr = await allsettled(iterable.map((it) => it.fn()))
+  const resArr = await allsettled(
+    iterable.map((it, i) => {
+      if (!it.fallbackFn) {
+        return it.fn()
+      }
+      return Promise.resolve(it.fn()).catch((err) => {
+        db(`index ${i} call origin fn error: `, err, 'called fallbackFn')
+        return it.fallbackFn()
+      })
+    })
+  )
   const res = []
   for (let i = 0; i < resArr.length; i++) {
     const r = resArr[i]
     if (r.status === 'fulfilled') {
       res.push(r.value)
     } else {
-      if (typeof iterable[i].fallbackFn === 'function') {
-        const fallbackRes = await iterable[i].fallbackFn()
-        db(
-          `index ${i} call origin fn error: `,
-          r.reason,
-          'called fallbackFn: ',
-          fallbackRes
-        )
-        res.push(fallbackRes)
-      } else {
-        db(`index ${i} has no fallbackFn, will throw: `, r.reason)
-        throw new Error(r.reason as any)
-      }
+      db(`index ${i} cause error, will throw: `, r.reason)
+      throw new Error(r.reason as any)
     }
   }
   return (res as any) as Promise<ResultTuple<T>>
